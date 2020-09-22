e5cef14b98aabd7f8de72d71cef4564042361b6d
Oussama EL Moatamid <oussamaelmoatamid@gmail.com>
2020-09-22 22:10:32 +0100

    Mini-Max Sum | Insert a Node at the Tail of a Linked List | Cutting Paper Squares | Weather Observation Station 4

---
+++ Mathematics/Cutting Paper Squares.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the solve function below.
+function solve(n, m) {
+    return BigInt(n-1)+BigInt(n)*BigInt(m-1);
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nm = readLine().split(' ');
+
+    const n = parseInt(nm[0], 10);
+
+    const m = parseInt(nm[1], 10);
+
+    const result = solve(n, m);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
