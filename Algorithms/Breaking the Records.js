395a397545d6e824a1e5832e7dc91c9a9f6e720a
Oussama EL Moatamid <oussamaelmoatamid@gmail.com>
2020-10-02 23:58:16 +0100

    Breaking the Records | Merge two sorted linked lists | Diwali Lighs | Weather Observation Station 11

---
+++ Algorithms/Breaking the Records.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the breakingRecords function below.
+function breakingRecords(scores) {
+    let lowestBreaks=0, biggestBreaks=0;
+    let min=scores[0], max=scores[0];
+
+    for(let i=1;i<scores.length;i++){
+        if(scores[i]<min){min=scores[i]; lowestBreaks++;}
+        else if(scores[i]>max){max=scores[i]; biggestBreaks++;}
+    }
+
+    return [biggestBreaks,lowestBreaks];
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const scores = readLine().split(' ').map(scoresTemp => parseInt(scoresTemp, 10));
+
+    const result = breakingRecords(scores);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
